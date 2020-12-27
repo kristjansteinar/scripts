81220193604f3824aad47300fa2cf83fab7b035a
kristjan <kristjan@gmail.com>
2020-12-27 01:29:34 +0000

    Adding env variables

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,44 @@
 #!/bin/sh
 
-export TEST=testing
+set -e
 
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [[ ${!var:-} && ${!fileVar:-} ]]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [[ ${!var:-} ]]; then
+        val="${!var}"
+    elif [[ ${!fileVar:-} ]]; then
+        val="$(< "${!fileVar}")"
+    fi
+
+    if [[ -n $val ]]; then
+        export "$var"="$val"
+    fi
+
+    unset "$fileVar"
+}
+
+# Database configuration variables
+file_env "DB_USERNAME"
+file_env "DB_PASSWORD"
+ile_env "DB_HOST"
+file_env "DB_PORT"
+
+# Keycloak integration variables
+file_env "KEYCLOAK_CLIENT_ID"
+file_env "KEYCLOAK_SECRET"
+file_env "KEYCLOAK_TLS_VERIFICATION"
+file_env "KEYCLOAK_HOST"
+file_env "KEYCLOAK_PORT"
+
+# Keycloak authentication string for password grant
+file_env "KEYCLOAK_TEST_LOGIN"
+
+# Start the server
 exec /deployments/run-java.sh
